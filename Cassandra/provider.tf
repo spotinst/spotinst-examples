736d7c97d11ccb086045b840ae2b4d0e41615c33
Ben Kiani <ben.kiani@spotinst.com>
2019-06-12 17:29:43 +0300

    remove tf files

--- Cassandra/provider.tf
+++
@@ -1,9 +0,0 @@
-# Configure the Spotinst provider
-provider "spotinst" {
-   token   = "${var.spotinst_token}"
-   account = "${var.spotinst_account}"
-}
-
-provider "aws" {
-  region = "${var.region}"
-}
