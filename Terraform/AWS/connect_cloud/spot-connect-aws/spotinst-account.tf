e680e725228f4f696492753ea87c7e222f7782bf
Steven Feltner <steven.feltner@netapp.com>
2021-06-10 11:55:14 -0700

    update

--- Terraform/AWS/connect_cloud/spot-connect-aws/spotinst-account.tf
+++ Terraform/AWS/connect_cloud/spot-connect-aws/spotinst-account.tf
@@ -2,7 +2,7 @@ module "spot_account" {
     source = "spot-account"
     #Desired Name of the Spot Account
     name = "test-terraform"
-    token = "c09767fd287c6c0df90a4eeba2380c34e248cd02faee419f81ee7b7be795a52f"
+    token = ""
 }
 
 output "spot_account_id" {
