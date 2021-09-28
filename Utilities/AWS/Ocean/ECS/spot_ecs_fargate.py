5af3c77264564062ee507103c38f0f4d4f3dbf5c
Steven Feltner <steven.feltner@netapp.com>
2021-09-28 10:28:14 -0700

    Added command line arguments for rename_services

--- Utilities/AWS/Ocean/ECS/spot_ecs_fargate.py
+++ Utilities/AWS/Ocean/ECS/spot_ecs_fargate.py
@@ -1,14 +1,7 @@
 #########################################
 ##  Written by steven.feltner@spot.io
-## This script should be ran BEFORE a spotinst ECS service import.
-## The script will gather all services and do the following:
-## 1) Describe all services within specified ECS cluster
-## 2) Gather all SGs for each service
-## 3) Create new Master SG
-## 4) Copy all rules from existing SGS into master SG
-## 5) Create a Spot Ocean Cluster
-## 6) Copy/Import all fargate services into EC2 services (This does not remove/delete original fargate services)
 ## NOTE: Please use at your own discretion. Not responsible for any damage or accidental deletion of AWS infrastructure.
+#########################################
 
 import base64
 import json
@@ -93,12 +86,12 @@ def cli(ctx, *args, **kwargs):
     help='VPC ID'
 )
 @click.option(
-    '--subnet',
+    '--subnet_ids',
     '-s',
     cls=PythonLiteralOption,
-    default='[]',
+    default=[],
     required=True,
-    help='Security group ids - Syntax: \'["sg-123456","sg-123456"]\''
+    help='Subnet ids - Syntax: \'["subnet-123456","subnet-123456"]\''
 )
 @click.option(
     '--iam_instance_role',
@@ -152,12 +145,73 @@ def import_fargate(ctx, *args, **kwargs):
                       secret_key=kwargs.get('secret_key'), session_token=kwargs.get('session_token'))
     oceanid = create_ocean(account_id=kwargs.get('account_id'), token=kwargs.get('token'),
                            ecs_cluster=kwargs.get('ecs_cluster'), region=kwargs.get('region'),
-                           subnet_id=kwargs.get('subnet_id'), ecs_ami_id=kwargs.get('ecs_ami'), sg_id=sg_id,
+                           subnet_ids=kwargs.get('subnet_ids'), ecs_ami_id=kwargs.get('ecs_ami'), sg_id=sg_id,
                            iam_instance_role=kwargs.get('iam_instance_role'))
     fargate_import(profile_name=kwargs.get('profile'), region=kwargs.get('region'), access_key=kwargs.get('access_key'),
                    secret_key=kwargs.get('secret_key'), session_token=kwargs.get('session_token'),
                    ecs_cluster=kwargs.get('ecs_cluster'), ocean_id=oceanid, account_id=kwargs.get('account_id'),
                    token=kwargs.get('token'))
+
+
+@cli.command()
+@click.option(
+    '--ecs_cluster',
+    '-e',
+    type=str,
+    required=True,
+    help='Name of the ECS cluster'
+)
+@click.option(
+    '--region',
+    '-r',
+    type=str,
+    required=True,
+    help='Region where the ECS cluster is located'
+)
+@click.option(
+    '--skip_nonrunning',
+    type=bool,
+    default=False,
+    help='Decide if we skip services that currently have no running tasks'
+)
+@click.option(
+    '--profile',
+    type=str,
+    required=False,
+    help='AWS CLI Profile Name'
+)
+@click.option(
+    '--access_key',
+    type=str,
+    required=False,
+    help='Access key for AWS CLI access'
+)
+@click.option(
+    '--secret_key',
+    type=str,
+    required=False,
+    help='Secret key for AWS CLI access'
+)
+@click.option(
+    '--session_token',
+    type=str,
+    required=False,
+    help='Session token for AWS CLI access'
+)
+@click.pass_context
+def rename_fargate(ctx, *args, **kwargs):
+    """## The script will look for any migrated (fargate -> EC2) services starting with "sfm" and do the following:
+## 1) Scale Original Fargate service to zero
+## 2) Delete Original Fargate service
+## 3) Copy configs of migrated EC2 service task definition
+## 4) Create new EC2 service with same name as Original Fargate service
+## 5) Scale Migrated "sfm" service to zero
+## 6) Delete migrated "sfm" service
+## NOTE: Please use at your own discretion. Not responsible for any damage or accidental deletion of AWS infrastructure."""
+    rename_services(ecs_cluster=kwargs.get('ecs_cluster'), region=kwargs.get('region'),
+                    skip_nonrunning=kwargs.get('skip_nonrunning'),
+                    profile_name=kwargs.get('profile_name'), access_key=kwargs.get('access_key'),
+                    secret_key=kwargs.get('secret_key'), session_token=kwargs.get('session_token'))
 
 
 ####################
@@ -251,7 +305,7 @@ def create_sg(ecs_cluster, vpc, region, sg_name, sg_description, profile_name, a
         # Add all rules to new security group
         for i in range(len(ip_permissions)):
             try:
-                client.authorize_security_group_ingress(GroupId=sg_id, IpPermissions=ip_permissions[i])
+                client.authorize_security_group_ingress(GroupId=str(sg_id), IpPermissions=ip_permissions[i])
             except ClientError as e:
                 if e.response['Error']['Code'] == 'InvalidPermission.Duplicate':
                     print(e.response['Error']['Message'] + " - Skipping")
@@ -303,7 +357,7 @@ def create_sg(ecs_cluster, vpc, region, sg_name, sg_description, profile_name, a
 #########################################
 ## The will create an ECS Ocean Cluster:
 #########################################
-def create_ocean(account_id, token, ecs_cluster, region, subnet_id, ecs_ami_id, sg_id, iam_instance_role):
+def create_ocean(account_id, token, ecs_cluster, region, subnet_ids, ecs_ami_id, sg_id, iam_instance_role):
     ocean_exist = False
 
     headers = {'Authorization': 'Bearer ' + token}
@@ -357,9 +411,7 @@ def create_ocean(account_id, token, ecs_cluster, region, subnet_id, ecs_ami_id, 
                     "drainingTimeout": 120
                 },
                 "compute": {
-                    "subnetIds": [
-                        subnet_id
-                    ],
+                    "subnetIds": subnet_ids,
                     "optimizeImages": {
                         "shouldOptimizeEcsAmi": 'true',
                         "performAt": "always"
@@ -438,7 +490,7 @@ def create_ocean(account_id, token, ecs_cluster, region, subnet_id, ecs_ami_id, 
                     "launchSpecification": {
                         "imageId": ecs_ami_id,
                         "userData": encoded_user_data,
-                        "securitygroupids": [
+                        "securityGroupIds": [
                             sg_id
                         ],
 
@@ -560,6 +612,125 @@ def fargate_import(profile_name, region, access_key, secret_key, session_token, 
                         print('Current migration status: ' + status)
                 else:
                     print(r_text)
+
+
+#########################################
+# rename fargate services
+#########################################
+def rename_services(ecs_cluster, region, skip_nonrunning, profile_name, access_key, secret_key, session_token):
+    try:
+        session = boto3.session.Session(profile_name=profile_name)
+        client = session.client('ecs', region_name=region)
+    except ProfileNotFound as e:
+        print(e)
+        print("Trying without profile...")
+        try:
+            client = boto3.client('ecs', region_name=region)
+        except ClientError as e:
+            client = boto3.client('ecs', region_name=region,
+                                  aws_access_key_id=access_key,
+                                  aws_secret_access_key=secret_key,
+                                  aws_session_token=session_token)
+
+    services = client.list_services(cluster=ecs_cluster, maxResults=100)
+
+    print('---------------------')
+
+    # Original service Names
+    service_names = []
+    # Migrated service Names
+    service_list = []
+
+    for i in services['serviceArns']:
+        # If using newer services it will be the second element
+        try:
+            service_names.append(i.split('/', 2)[2])
+        # If using legacy services it will be the first element
+        except:
+            service_names.append(i.split('/', 2)[1])
+
+    # Only get services that start with "sfm"
+    for j in service_names:
+        if j.startswith('sfm'):
+            response = client.describe_services(cluster=ecs_cluster, services=[j])
+            current_runningcount = response['services'][0]['runningCount']
+            if skip_nonrunning:
+                if current_runningcount == 0:
+                    print("SKIPPING - service " + j + " has no tasks running")
+                else:
+                    service_list.append(j)
+            else:
+                service_list.append(j)
+        else:
+            pass
+
+    # Get the service names for the migrated services. Convert back to original name
+    service_names = []
+    for i in service_list:
+        service = i.split('-', 1)[1]
+        service_names.append(service)
+
+    # delete original fargate services
+    for i in range(len(service_names)):
+        print("Updating orginal Fargate service to Zero: " + service_names[i])
+        try:
+            client.update_service(cluster=ecs_cluster, service=service_names[i], desiredCount=0)
+            task = client.list_tasks(cluster=ecs_cluster, serviceName=service_names[i])
+            for x in task['taskArns']:
+                task_name = (x.split('/', 1)[1])
+                print("Stopping task: " + task_name)
+                client.stop_task(cluster=ecs_cluster, task=task_name)
+        except ClientError as e:
+            print(e)
+
+        print("Deleting original Fargate service: " + service_names[i])
+        try:
+            client.delete_service(cluster=ecs_cluster, service=service_names[i])
+        except ClientError as e:
+            print(e)
+
+    # wait for the services to be deleted before creating new ones with the same name
+    time.sleep(60)
+
+    # describe the services from the cluster with a max of 10 at a time
+    # divide the list into chunks of 1
+    n = 1
+    x = list(divide_chunks(service_list, n))
+
+    # Get the configurations from the migrated services and create new service with corrected/original name
+    for i in range(len(x)):
+        services = client.describe_services(cluster=ecs_cluster, services=x[i])
+        temp = services.get('services')
+        dict = temp[0]
+        print("Creating EC2 Service: " + service_names[i])
+        try:
+            client.create_service(cluster=ecs_cluster, serviceName=service_names[i],
+                                  taskDefinition=dict.get('taskDefinition'),
+                                  loadBalancers=dict.get('loadBalancers'),
+                                  serviceRegistries=dict.get('serviceRegistries'),
+                                  desiredCount=dict.get('desiredCount'), launchType=dict.get('launchType'),
+                                  deploymentConfiguration=dict.get('deploymentConfiguration'),
+                                  placementConstraints=dict.get('placementConstraints'),
+                                  placementStrategy=dict.get('placementStrategy'),
+                                  networkConfiguration=dict.get('networkConfiguration'),
+                                  schedulingStrategy=dict.get('schedulingStrategy'))
+        except ClientError as e:
+            print(e)
+
+    # delete Migrated EC2 services with sfm
+    for i in range(len(service_list)):
+        print("Deleting Migrated SFM Service: " + service_list[i])
+        try:
+            client.update_service(cluster=ecs_cluster, service=service_list[i], desiredCount=0)
+        except ClientError as e:
+            print(e)
+        try:
+            client.delete_service(cluster=ecs_cluster, service=service_list[i])
+        except ClientError as e:
+            print(e)
+
+    print("Completed")
+    print('---------------------')
 
 
 if __name__ == "__main__":
