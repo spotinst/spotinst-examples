9ae959666aabfef51cda016d65c3f0b574ccf11d
Scott Sullivan <126610240+sc0tt-sullivan@users.noreply.github.com>
2024-04-02 17:28:17 -0400

    More cleanup.

--- Utilities/AzureOnboardingCLI/azure-automatic-role-assignment.py
+++ Utilities/AzureOnboardingCLI/azure-automatic-role-assignment.py
@@ -413,7 +413,7 @@ def get_or_create_custom_role(custom_role_name, custom_role_json_local_path, sub
             custom_role_name, custom_role_json_local_path, subscription
         )
 
-        # the spot core product custom role is defined in the REST API format (slightly different  than the CLI format)
+        # the spot core product custom role is defined in the REST API format (slightly different from the CLI format)
         # so use the az rest command
         custom_role_id = uuid.uuid4()
         create_custom_role_cmd = "az rest --method put"
@@ -422,15 +422,16 @@ def get_or_create_custom_role(custom_role_name, custom_role_json_local_path, sub
             "--url", f"https://management.azure.com/subscriptions/{subscription}/providers/Microsoft.Authorization/roleDefinitions/{custom_role_id}?api-version=2022-04-01",
             "--body", custom_role
         )
+        result = create_custom_role_response["properties"]["roleName"]
 
         ''' If using a custom role in the CLI format, use the `az role definition create` command
         create_custom_role_cmd = "az role definition create"
         create_custom_role_response = run_command(
             create_custom_role_cmd, "--role-definition", custom_role
         )
+        result = create_custom_role_response["roleName"]
         '''
 
-        result = create_custom_role_response["properties"]["roleName"]
         log(f"Finished to create custom role: {result}")
     else:
         result = get_custom_role_response[0]["roleName"]
